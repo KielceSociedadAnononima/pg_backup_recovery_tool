a45aef1b89b16ed6b4e13eb636ef92bce7cfa14a
Ignacio Perez Panizza <nachopp98@hotmail.com>
2024-10-11 01:36:52 -0300

    flow control of tool

--- src/backup_postgres.rs
+++ src/backup_postgres.rs
@@ -1,20 +1,29 @@
 
-use std::process::Command;
+use std::process::{exit, Command};
 use crate::pg_db_model::PostgresDB;
 
+#[allow(dead_code)]
 pub fn generate_dump_all(model_db: PostgresDB)
 {
     let program = format!("{}\\{}",model_db.folder_instance, "pg_dumpall.exe");
     match Command::new(program)
-        .arg("-U postgres")
-        .arg(format!("-P {}",model_db.port))
-        .arg(format!("-f {}.respaldo_modulo.sql",model_db.folder_output))
+        .arg("-U")
+        .arg("postgres")
+        .arg("-P")
+        .arg(model_db.port.to_string())
+        .arg("-f")
+        .arg(format!("{}.respaldo_modulo.sql",model_db.folder_output))
         .spawn() {
-        Ok(res) => {
-            
+        Ok(mut res) => {
+            let estado = res.wait();
+            if estado.is_ok() {
+                exit(0)
+            }
+            exit(1)
         },
         Err(err) => {
-
+            eprint!("{:?}",err);
+            exit(1)
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,59 @@
+use clap::{command, Arg, ArgAction, Command};
+
 mod backup_postgres;
 mod pg_db_model;
 
 fn main()
 {
-    
+    /**************************************************************************** */
+    /***********************  Evaluo los comandos de la cli  ******************** */
+    /**************************************************************************** */
+    //
+    let opciones = command!()
+    .subcommand( 
+        Command::new("backup")
+        .about("Genera un dump all de la base de datos.")
+        .short_flag('b')
+        .arg(
+            Arg::new("config")
+                .action(ArgAction::Append)
+                .required(true)
+
+        )
+    )
+    .subcommand(
+        Command::new("restore")
+        .about("Restaura un dump all desde el archivo. Elimina las dos bases de produccion antes de restaurar.EL MODULO LOCAL DEBE ESTAR CERRADO.")
+        .short_flag('r')
+        .arg(
+            Arg::new("config")
+            .action(ArgAction::Append)
+            .required(true)
+        )
+    )
+    .get_matches();
+
+    /**************************************************************************** */
+    /***********************  Redirecciono a la funcion  ************************ */
+    /**************************************************************************** */
+    match opciones.subcommand() {
+        Some(("backup", sub_m)) => {
+            if let Some(_ruta) = sub_m.get_one::<String>("config"){
+
+            }else {
+                eprintln!("Ingrese un parametro valido. Corra el programa con -h para ver las opciones.");
+            }
+        }
+        Some(("restore", sub_m)) => {
+            if let Some(_ruta) = sub_m.get_one::<String>("config"){
+
+            }else {
+                eprintln!("Ingrese un parametro valido. Corra el programa con -h para ver las opciones.");
+            }
+        }
+        _ => {
+            eprintln!("Ingrese un parametro valido. Corra el programa con -h para ver las opciones.");
+        }
+    }
 
 }
